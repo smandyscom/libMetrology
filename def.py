a8626848a61dc25b04abc16350b161a576171b5f
smandyscom <smandyscom@gmail.com>
2016-10-17 01:14:46 +0800

    symbol function added

--- def.py
+++ def.py
@@ -1,13 +1,18 @@
 from sympy import *
 import unittest
 
-def HomogenousTransformation(subscript):
+def HomogenousTransformationSymbol(subscript):
     R = MatrixSymbol('R' + subscript,3,3)
     P = MatrixSymbol('P' + subscript,3,1)
     zero = ZeroMatrix(1,3)
     one = Identity(1)
     return Matrix([[R,P],[zero,one]])
 
-# init_session()
-print Matrix(HomogenousTransformation('cs') * HomogenousTransformation('sr'))
+def PositionSymbol(subscript):
+    pass
 
+def VectorSymbol(subscript):
+    pass
+
+if __name__ == '__main__':
+    print Matrix(HomogenousTransformationSymbol('cs') * HomogenousTransformationSymbol('sr'))
