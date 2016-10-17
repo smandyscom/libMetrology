6e724e03b8373b34e6d955a224a95888c31f3d43
smandyscom <smandyscom@gmail.com>
2016-10-17 19:27:58 +0800

    .

--- def.py
+++ def.py
@@ -14,7 +14,5 @@ def PositionSymbol(subscript):
 def VectorSymbol(subscript):
     pass
 
-print "Debugging"
 if __name__ == '__main__':
-    print "Debugging.."
     print Matrix(HomogenousTransformationSymbol('cs') * HomogenousTransformationSymbol('sr'))
