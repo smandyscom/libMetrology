e8efca4aa68c37624c8e2f88b6f29dfa133de255
smandyscom <smandyscom@gmail.com>
2016-10-18 19:30:38 +0800

    use ikpy as rotation matrix

--- def2.py
+++ def2.py
@@ -1,12 +1,21 @@
 from numpy import *
+from ikpy import geometry_utils
 #import numpy
 
 class HomogenousTransformation(matrix):
-    def __init__(self,*args,**kwargs):
-        super(HomogenousTransformation,self).__init__(*args,**kwargs)
+    def __new__(self,tx=0,ty=0,tz=0,alpha=0,beta=0,gamma=0):
+        #Fix me : use keyword args , xyzabc , RP(Rotation,Translation) , 
+        #
+        # Euler sequence : translate xyz -> rotate about x alpha radians -> rotate about y 
+        # 
+        self = identity(4)
+        return self
+    def __init__(self):
+        self.__R=self[0:3,0:3]
+        self.__P=self[0:3,3]
+    #def __init__(self,*args,**kwargs):
+        #super(HomogenousTransformation,self).__init__(*args,**kwargs)
         #given artibary matrix , check properties of HTM
-#        self.__R=self[0:3,0:3]
-#        self.__P=self[0:3,3]
         
     def is_valid(self):
         #check out if the matrix satisfied HTM properties
@@ -24,11 +33,12 @@ class HomogenousTransformation(matrix):
         return not QTQ.any() 
 
 class PositionVector(matrix):
-    def __init__(self,*args,**kwargs):
-        super(PositionVector,self).__init__(*args,**kwargs)
+    def __new__(self,px=0.,py=0.,pz=0.,scale=1.):
+        self = matrix([px,py,pz,scale])
+        return self.T
 
 if __name__ == '__main__':
-    H = HomogenousTransformation([[1,2],[3,4]])
+    H = HomogenousTransformation()
     print H 
-    P = PositionVector([0,0,0,1])
-    print P
+    P = PositionVector(0,0,0,1)
+    print P 
