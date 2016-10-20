df13b2fa623a59d54ae27b9dc465fae9bc9c6cc7
smandyscom <smandyscom@gmail.com>
2016-10-20 10:41:25 +0800

    set assert to make sure precondition is fine

--- phase1.py
+++ phase1.py
@@ -3,6 +3,12 @@ import numpy
 from numpy import random as rd
 PI=numpy.pi
 
+#given 4-point , generate vector/postion  set respect to p0
+def vp_set(p_set):
+    vp_set_ = numpy.hstack(tuple([p_set[:,i]-p_set[:,0] for i in range(1,4)]))
+    vp_set_ = numpy.matrix(numpy.hstack((vp_set_,p_set[:,0])))
+    return vp_set_
+
 p_s = def2.PositionVector(0, 0, 10)
 
 
@@ -20,61 +26,46 @@ position_error_magnitude = 5 #10 mm
 position_error= tuple([rd.sample()*position_error_magnitude for x in range(0,3)])
 angle_error_magnitude = numpy.deg2rad(5)# rad
 angle_error = tuple([rd.sample()*angle_error_magnitude for x in range(0,3)])
-error_vector = position_error+angle_error
+error_vector = position_error+angle_error # tuple join
 print 'Error vector:{0}'.format(error_vector)
 T_c_r_error = def2.HomogenousTransformation(xyzabc=error_vector)
 T_c_r = T_c_r_error * T_c_r_nominal
 
 #generate 4-points able to construct orthogonal vector, respect to frame-r
 p_set_r = numpy.hstack(tuple([T_s0_r(0)*T_s_s0(p+(0,0,0))*p_s for p in [(0,0,0),(1,0,0),(0,1,0),(0,0,1)]]))
-print p_set_r
-
-# [p1-p0 , p2-p0 , p3-p0 , p0 ]j
-vp_set_r = numpy.hstack(tuple([p_set_r[:,i]-p_set_r[:,0] for i in range(1,4)]))
-vp_set_r = numpy.matrix(numpy.hstack((vp_set_r,p_set_r[:,0])))
-print vp_set_r
-
+# [p1-p0 , p2-p0 , p3-p0 , p0 ]r
+vp_set_r = vp_set(p_set_r) 
 # observed by frame-C
 p_set_c = T_c_r.I * p_set_r
-print 'p_set_c{0}'.format(p_set_c)
-#
+# [p1-p0 , p2-p0 , p3-p0 , p0 ]
+vp_set_c = vp_set(p_set_c)
 
-#given 4-point , generate vector/postion  set respect to p0
-def vp_set(p_set):
-    vp_set_ = numpy.hstack(tuple([p_set[:,i]-p_set[:,0] for i in range(1,4)]))
-    vp_set_ = numpy.matrix(numpy.hstack((vp_set_,p_set[:,0])))
-    return vp_set_
-
-print 'vp_set_c{0}'.format(T_c_r.I * vp_set(p_set_r))
-print 'vp_set_c_1{0}'.format(vp_set(p_set_c))
-print 'T_c_r.I_1{0}'.format(vp_set(p_set_c)*vp_set(p_set_r).I)
-print 'T_c_r.I_2{0}'.format(T_c_r.I)
+assert numpy.allclose(T_c_r.I*vp_set_r,vp_set_c) , 'vp_set_c equals T_c_r.I* vp_set_r'
+assert numpy.allclose(vp_set_c*vp_set_r.I,T_c_r.I) , 'T_c_r.I equals vp_set_c * vp_set_r.I'
 
 #take x,y part only , since it is CCD
 p_set_c_xy = p_set_c[0:2,:]
 vp_set_c_xy = vp_set(p_set_c_xy)
-# vp_set_c_xy = numpy.hstack(tuple([p_set_c_xy[:,i]-p_set_c_xy[:,0] for i in range(1,4)]))
-# vp_set_c_xy = numpy.hstack((vp_set_c_xy,p_set_c_xy[:,0]))
-# print vp_set_c_xy
+
+#Object : given vp_set_c_xy , restore T_c_r
+# algorithm need to test :Start
 
 # due to property of orthogonal , cross product Vx,Vy part to generate Vz
 vx = vp_set_c_xy[0,0:3]
 vy = vp_set_c_xy[1,0:3]
 vz = numpy.matrix(numpy.cross(vx,vy))
-print vx,vy,vz
 vx_vy_vz = numpy.matrix(numpy.vstack((vx,vy,vz)))
-print vx_vy_vz
-print def2.is_Orthogonal(vx_vy_vz)
+assert def2.is_Orthogonal(vx_vy_vz)
 
-vp_set_c = numpy.matlib.identity(4)
-vp_set_c[0:3,0:3] = vx_vy_vz
+vp_set_c_answer = numpy.matlib.identity(4)
+vp_set_c_answer[0:3,0:3] = vx_vy_vz
 #Fix me : how to get z value
 vp_set_c[:,3]=p_set_c[:,0]
 
 
 print 'vp_set_c_1{0}'.format(vp_set(p_set_c))
 print 'vp_set_c_2{0}'.format(vp_set_c)
-import pdb; pdb.set_trace()  # XXX BREAKPOINT
+#import pdb; pdb.set_trace()  # XXX BREAKPOINT
 
 T_r_c = numpy.matlib.identity(4)
 T_r_c[0:3,0:3] = vp_set_c[0:3,0:3] * vp_set_r[0:3,0:3].I
@@ -83,7 +74,7 @@ T_c_r_answer = T_r_c.I
 
 print 'T_c_r_answer{0}'.format(T_c_r_answer)
 print 'T_c_r{0}'.format(T_c_r)
-import pdb; pdb.set_trace()  # XXX BREAKPOINT
+#import pdb; pdb.set_trace()  # XXX BREAKPOINT
 
 print 'T_c_r_error_answer.I{0}'.format(vp_set_c * (T_c_r_nominal.I*vp_set_r).I)
 print 'T_c_r_error.I{0}'.format(T_c_r_error.I)
