192642f81286d72f948abbc9477023c2b85bbf03
smandyscom <smandyscom@gmail.com>
2016-10-23 18:39:24 +0800

    updated

--- symbol_def.py
+++ symbol_def.py
@@ -1,13 +1,28 @@
+#!/usr/bin/env python
 from sympy import *
 import unittest
 
+global aa
+aa = 5
 
-# def Translation_Matrix
+def Transformation_Matrix(*args):
+    R,P = args
+    htm = eye(4)
+    htm[0:3,0:3] = R
+    htm[0:3,3] = P
+    return htm
+
+
+def Translation_Vector(*args):
+    x,y,z = symbols(args)
+    return Matrix([x,y,z])
+
 
 def Rz_Matrix(theta):
     return Matrix([[cos(theta),-sin(theta),0],
     [sin(theta),cos(theta),0],
     [0,0,1]])
+
 
 def Ry_Matrix(theta):
     return Matrix([[cos(theta),0,sin(theta)],
@@ -36,25 +51,33 @@ def Error_Matrix(*args):
     return expr
 
 if __name__ == '__main__':
-    print Rz_Matrix('c')*Ry_Matrix('b')*Rx_Matrix('a')
-    H = Ry_Matrix('b')*Rx_Matrix('a')
-    print H
-    print simplify(H)
-    print trigsimp(simplify(H))
-    # small angle approch
-    for ch in ['a','b']:
-        H = H.subs(sin(ch),ch)
-        H = H.subs(cos(ch),1)
-    print simplify(H)
-    for ch in ['a','b']:
-        H = H.subs(ch,3.14/180)
-    print H
-    print type(H)
-    print H.evalf()
-    print type(H.evalf())
+    init_session()
 
 class TestSymbolFunction(unittest.TestCase):
     def setUp(self):
         init_printing()
+    def test_ad_hoc(self):
+        print Rz_Matrix('c')*Ry_Matrix('b')*Rx_Matrix('a')
+        H = Ry_Matrix('b')*Rx_Matrix('a')
+        print H
+        print simplify(H)
+        print trigsimp(simplify(H))
+        # small angle approch
+        for ch in ['a','b']:
+            H = H.subs(sin(ch),ch)
+            H = H.subs(cos(ch),1)
+        print simplify(H)
+        for ch in ['a','b']:
+            H = H.subs(ch,3.14/180)
+        print H
+        print type(H)
+        print H.evalf()
+        print type(H.evalf())
+
     def test_Error_Matrix(self):
         print Error_Matrix('a','b','c')
+    def test_Translation_Vector(self):
+        print Translation_Vector('x','y','z')
+    def test_Transformation_Matrix(self):
+        print Transformation_Matrix(Error_Matrix('a','b','c'),
+                                    Translation_Vector('x','y','z'))
