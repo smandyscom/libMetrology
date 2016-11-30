d5e06d48e7446bab78ea4a41f849a2c433db5678
smandyscom <smandyscom@gmail.com>
2016-11-30 23:28:54 +0800

    frame notation renamed

--- phase_laser_calibration_symbol_2.py
+++ phase_laser_calibration_symbol_2.py
@@ -12,19 +12,20 @@ LX,LY,LZ = sympy.symbols(('LX','LY','LZ'))
 #Error factors for Frame L to Frame Y0
 elx,ely,slx,sly,slz = sympy.symbols(('elx','ely','slx','sly','slz'))
 
-T_L_Y0_error = symbol_def.Transformation_Matrix(symbol_def.Error_Matrix('elx', 'ely', 0),
+# Mearsure frame = YE = L , yaw is dont cared , since it is distance meter
+T_YE_Y = symbol_def.Transformation_Matrix(symbol_def.Error_Matrix('elx', 'ely', 0),
                                                 symbol_def.Translation_Vector('slx', 'sly','slz'))
-T_L_Y0_nominal = symbol_def.Transformation_Matrix(sympy.eye(3),
-                                                  symbol_def.Translation_Vector('LX', 'LY', 'LZ'))
-T_Y0_S0_error = symbol_def.Transformation_Matrix(symbol_def.Error_Matrix('eyx', 'eyy','eyz'),
+T_Y_Y0E = symbol_def.Transformation_Matrix(sympy.eye(3),
+                                                  symbol_def.Translation_Vector(0, 'LY', 0))
+T_Y0E_Y0 = symbol_def.Transformation_Matrix(symbol_def.Error_Matrix('eyx', 'eyy','eyz'),
                                                  symbol_def.Translation_Vector('syx', 'syy', 'syz'))
 
 p_l_measure = symbol_def.Translation_Vector(0,0,'zl').col_join(sympy.Matrix([1]))
 p_s0_known = symbol_def.Translation_Vector('rx','ry','rz').col_join(sympy.Matrix([1]))
 
-p_s0_measure = T_Y0_S0_error *T_L_Y0_nominal *T_L_Y0_error *p_l_measure
+p_s0_measure = T_Y0E_Y0 *T_Y_Y0E *T_YE_Y *p_l_measure
 
-linkage = sympy.Eq(T_Y0_S0_error *T_L_Y0_nominal *T_L_Y0_error *p_l_measure,p_s0_known)
+linkage = sympy.Eq(T_Y0E_Y0 *T_Y_Y0E *T_YE_Y *p_l_measure,p_s0_known)
 print linkage
 
 #ignore any second error, take all of them as zero
@@ -57,7 +58,7 @@ print p_s0_measure
 #estimate the condition
 #p_l_measures = sympy.Matrix([[0,0,'zl'+str(x),1] for x in range(0,4)]).T
 #p_s0_knowns = sympy.Matrix([['rx'+str(x),'ry'+str(x),'rz'+str(x),1] for x in range(0,4)]).T
-#p_s0_measures= T_Y0_S0_error *T_L_Y0_nominal *T_L_Y0_error *p_l_measures 
+#p_s0_measures= T_Y0E_Y0 *T_Y_Y0E *T_YE_Y *p_l_measures 
 #print p_s0_measures
 #coeff_matrix = [polys.polytools.poly(p_s0_measures[i,j],(eyx,eyy,eyz,syx,syy,syz,elx,ely,slx,sly,slz)).coeffs() for i in range(0,4) for j in range(0,4)]
 #print sympy.Matrix(coeff_matrix)
