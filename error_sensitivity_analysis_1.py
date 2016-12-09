a8b3dcfa4655a0ce205d67a47ac5138e727adbc6
smandyscom <smandyscom@gmail.com>
2016-12-09 10:21:00 +0800

    let analysis to be a function

--- error_sensitivity_analysis_1.py
+++ error_sensitivity_analysis_1.py
@@ -1,27 +1,32 @@
 import symbol_def
 import sympy
+# C_FRAME = sympy.Matrix(([0, 0, 1],
+                           # [-1, 0, 0],
+                           # [0, 1, 0]))
+# S_FRAME = sympy.Matrix(([-1, 0, 0],
+                        # [0, 1, 0],
+                        # [0, 0, -1]))
 
+ERROR_VECTOR = sympy.Matrix(['ex', 'ey', 'ez', 'sx', 'sy', 'sz'])
+Px, Py, Pz = sympy.symbols(['Px', 'Py', 'Pz'])
 #CCD real frame to ideal frame
-#
 T_c_creal = symbol_def.Transformation_Matrix(symbol_def.Error_Matrix('ex', 'ey', 'ez'),
                                                 symbol_def.Translation_Vector('sx', 'sy','sz'))
-T_c_r = symbol_def.Transformation_Matrix(sympy.eye(3),
-                                         symbol_def.Translation_Vector('Cx','Cy','Cz'))
-T_s0_r = symbol_def.Transformation_Matrix(sympy.eye(3),
-                                          symbol_def.Translation_Vector('S0X','S0Y','S0Z'))
-#fixed rotation(reference frame)
-T_s_s0 = symbol_def.Transformation_Matrix(sympy.eye(3),
-                                         symbol_def.Translation_Vector('SX','SY','SZ'))
 
-#
-__chain = T_c_creal*T_c_r.inv()*T_s0_r*T_s_s0
-# print T_c_r.inv()
-print __chain[0,3]
-print __chain[2,3]
-__chain_p = __chain[0:2,3]
-sympy.init_printing()
-print __chain_p.jacobian(sympy.Matrix(['ex','ey','ez','sx','sy','sz']))
+def generate_analysis(C_FRAME, S_FRAME):
+    T_c_r = symbol_def.Transformation_Matrix(C_FRAME,
+                                            symbol_def.Translation_Vector('Cx','Cy','Cz'))
+    T_s0_r = symbol_def.Transformation_Matrix(S_FRAME,
+                                            symbol_def.Translation_Vector('S0X','S0Y','S0Z'))
+    #fixed rotation(reference frame)
+    T_s_s0 = symbol_def.Transformation_Matrix(sympy.eye(3),
+                                            symbol_def.Translation_Vector('SX','SY','SZ'))
+    #
+    P_s = sympy.Matrix([Px, Py, Pz, 1])
+    P_c = T_c_creal*T_c_r.inv()*T_s0_r*T_s_s0*P_s
+    P_c = P_c[0:2, :]
 
+    error_gain_p_c  = P_c.jacobian(ERROR_VECTOR)
+    return P_c, error_gain_p_c
 
-__chain_work = T_s_s0.inv()*T_s0_r.inv()*T_c_r *T_c_creal.inv()
 
--- error_sensitivity_analysis_1_numeric.py
+++ error_sensitivity_analysis_1_numeric.py
@@ -3,9 +3,17 @@ import numpy
 import random
 
 ERROR_SCALE = 0.0001
-VECTOR_C = (10, 10, 10)
-VECTOR_S0 = (10, 10, 10)
-VECTOR_P = (0, 0, 10)
+NOISE_SCALE = 0.1 * ERROR_SCALE
+VECTOR_C = (95.823, 203.615, 58.255)
+VECTOR_S0 = (95.823 + 65, 163.5, 120.002)
+VECTOR_P = (0, 0, 120.002 - 58.255)
+C_FRAME = numpy.matrix(([0, 0, 1],
+                           [-1, 0, 0],
+                           [0, 1, 0]))
+S_FRAME = numpy.matrix(([-1, 0, 0],
+                        [0, 1, 0],
+                        [0, 0, -1]))
+
 Cx, Cy, Cz = VECTOR_C
 S0x, S0y, S0z = VECTOR_S0
 Px, Py, Pz = VECTOR_P
@@ -22,9 +30,9 @@ error_vector_answer = (T_c_creal.item((2,1)),
                        T_c_creal.item((1,3)),
                        T_c_creal.item((2,3)))
 
-T_c_r = numeric_def.HomogenousTransformation(RP=(numpy.eye(3), numpy.matrix(VECTOR_C).T))
+T_c_r = numeric_def.HomogenousTransformation(RP=(C_FRAME, numpy.matrix(VECTOR_C).T))
 
-T_s0_r = numeric_def.HomogenousTransformation(RP=(numpy.eye(3), numpy.matrix(VECTOR_S0).T))
+T_s0_r = numeric_def.HomogenousTransformation(RP=(S_FRAME, numpy.matrix(VECTOR_S0).T))
 
 #fixed pose
 def T_s_s0(translation):
@@ -32,8 +40,8 @@ def T_s_s0(translation):
 
 def coefficien_matrix(vector_S):
     Sx, Sy, Sz = vector_S
-    return numpy.matrix(([0, -Cz+Pz+S0z+Sz, Cy-Py-S0y-Sy, 1, 0],
-                         [Cz-Pz-S0z-Sz, 0, -Cx+Px+S0x+Sx, 0, 1]))
+    return numpy.matrix(([0, -Cx-Px+S0x-Sx, Cz+Pz-S0z+Sz, 1, 0],
+                         [Cx+Px-S0x+Sx, 0, Cy-Py-S0y-Sy, 0, 1]))
 
 #the end ball used to do calibration
 P_s = numeric_def.PositionVector(Px, Py, Pz)
@@ -42,7 +50,11 @@ VECTOR_S_SET = [(0, 0, 0),
                 (10, 10, 1),
                 (-10, 10, 1),
                 (10, -10, 1),
-                (-10, -10, 1)]
+                (-10, -10, 1),
+                (10, 10, -1),
+                (-10, 10, -1),
+                (10, -10, -1),
+                (-10, -10, -1)]
 
 
 P_c_offseted = []
@@ -53,12 +65,15 @@ for index in range(0, len(VECTOR_S_SET)):
     Sx, Sy, Sz = VECTOR_S_SET[index]
     P_c = T_c_creal * T_c_r.I * T_s0_r * T_s_s0((Sx, Sy, Sz)) * P_s
     # offset
-    P_c = P_c - numpy.matrix((-Cx+S0x+Sx+Px,
-                              -Cy+S0y+Sy+Py,
+    P_c = P_c - numpy.matrix((Cy-S0y-Sy-Py,
+                              -Cz+S0z-Sz-Pz,
                               0,
                               1)).T
+    #adding some noise
+    P_c = P_c[0:2 , :] + numpy.matrix((random.uniform(-NOISE_SCALE,NOISE_SCALE),
+                                       random.uniform(-NOISE_SCALE,NOISE_SCALE))).T
     # print P_c
-    P_c_offseted.append(P_c[0:2, :])
+    P_c_offseted.append(P_c)
     # print P_c_offseted
     Coefficients.append(coefficien_matrix((Sx, Sy, Sz)))
 
