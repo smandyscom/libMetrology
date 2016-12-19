c8c20a168f2565e11a83c707063ae6d5bdb64fea
smandyscom <smandyscom@gmail.com>
2016-12-19 22:08:29 +0800

    map added proven of laser end

--- ci_error_analysis.py
+++ ci_error_analysis.py
@@ -7,9 +7,10 @@ def ccd_error_analysis(Ax, key='C1'):
     T_ci_r, T_ci_cireal, ERROR_VECTOR_Ci = frame_c_dictionary[key]
     #
     S_AXIS_VECTOR = ('Sx', 'Sy', 'Sz', 0, 0, 0)
-    P_ci_real = T_ci_cireal*T_ci_r.inv()*T_s0_r(Ax)*T_s_s0(S_AXIS_VECTOR)*P_r
     P_ci_nominal = T_ci_r.inv()*T_s0_r(Ax)*T_s_s0(S_AXIS_VECTOR)*P_r
+    P_ci_real = T_ci_cireal*P_ci_nominal
     P_ci_error = P_ci_nominal[0:2, :] - P_ci_real[0:2, :]
+
 
     error_gain_p_ci  = P_ci_error.jacobian(ERROR_VECTOR_Ci)
     return error_gain_p_ci
