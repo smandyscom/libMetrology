272cb1a170eaed4e09296f3af89b089a220936ba
smandyscom <smandyscom@gmail.com>
2016-12-26 11:55:15 +0800

    batch circle points generated

--- l_error_analysis_numeric.py
+++ l_error_analysis_numeric.py
@@ -1,4 +1,5 @@
 import numeric_def
+import frame_def_numeric
 import numpy
 import random
 
@@ -6,17 +7,18 @@ import random
 RADIUS = 2
 PARTS = 10
 RADS = [x*(numpy.pi/PARTS) for x in range(0, 10)]
-ROUNDS = map(lambda rad: (RADIUS*numpy.cos(rad), RADIUS*numpy.sin(rad), 0, 1),
+ROUNDS = map((lambda rad: (RADIUS*numpy.cos(rad), RADIUS*numpy.sin(rad), 0, 1)),
              RADS)
+ROUNDS = map(lambda __rounds : numpy.matrix(__rounds), ROUNDS)
 CENTERS = ((0, 0, 0, 1),
            (10, 0, 0, 1),
            (20, 0, 0, 1),
            (0, 10, 0, 1),
            (0, 20, 0, 1))
+CENTERS = map(lambda __center: numpy.matrix(__center), CENTERS)
 
 #circle round points to corresponding Ax,Ay , according to revealed chain
 circle_points = [map(lambda round_point: round_point + __center, ROUNDS)  for __center in CENTERS ]
-print ROUNDS
 #Ax,Ay fitted center Ax_bar,Ay_bar
 
 #real center represented in L
